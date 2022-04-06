ba8a10c49e30c21f647eb51b687d4a9d7d9f8f9c
handshakings <handshakings@gmail.com>
2022-04-06 14:31:53 +0500

    1.Create window service 2.Create Win form app to install/start/stop/uninstall service

---
+++ MyService/Service1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace MyService
+{
+    [RunInstaller(true)]
+    public partial class Service1 : ServiceBase
+    {
+        Thread worker = null;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                worker = new Thread(working);
+                worker.Start();
+            }
+            catch (Exception)
+            {
+                StreamWriter writer = new StreamWriter("e:\\abc.txt", true);
+                writer.WriteLine("Service Failed");
+                writer.Flush();
+                writer.Close();
+            }  
+        }
+
+        private void working()
+        {
+            StreamWriter writer = new StreamWriter("e:\\abc.txt", true);
+            while(true)
+            {
+                writer.WriteLine("Service is called on " + DateTime.Now.ToString());
+                writer.Flush();
+                writer.Close();
+                Thread.Sleep(5000);
+            }  
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                if (worker != null && worker.IsAlive)
+                {
+                    worker.Abort();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ WinServiceManager/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinServiceManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(DialogResult.OK == dialog.ShowDialog())
+            {
+                string winServiceExePath = dialog.FileName;
+                label1.Text = winServiceExePath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process std = new Process();
+            ProcessStartInfo stdInfo = new ProcessStartInfo();
+            stdInfo.FileName = "cmd.exe";
+            stdInfo.Arguments = @"/c sc.exe create MyService binPath=" + label1.Text;
+            stdInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            stdInfo.CreateNoWindow = true;
+            std.StartInfo = stdInfo;
+            std.Start();
+            std.Close();
+        }
+
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process std = new Process();
+            ProcessStartInfo stdInfo = new ProcessStartInfo();
+            stdInfo.FileName = "cmd.exe";
+            stdInfo.Arguments = @"/c sc.exe start MyService";
+            stdInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            stdInfo.CreateNoWindow = true;
+            std.StartInfo = stdInfo;
+            std.Start();
+            std.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Process std = new Process();
+            ProcessStartInfo stdInfo = new ProcessStartInfo();
+            stdInfo.FileName = "cmd.exe";
+            stdInfo.Arguments = @"/c sc.exe stop MyService";
+            stdInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            stdInfo.CreateNoWindow = true;
+            std.StartInfo = stdInfo;
+            std.Start();
+            std.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process std = new Process();
+            ProcessStartInfo stdInfo = new ProcessStartInfo();
+            stdInfo.FileName = "cmd.exe";
+            stdInfo.Arguments = @"/c sc.exe delete MyService";
+            stdInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            stdInfo.CreateNoWindow = true;
+            std.StartInfo = stdInfo;
+            std.Start();
+            std.Close();
+        }
+
+    }
+}
